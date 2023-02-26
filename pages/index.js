f054d35419b012d8b08e923f7b8f9016fa874f70
宮本　健 <kennyjourney525@gmail.com>
2023-02-27 01:25:55 +0900

    Homeページに誤って追加していたヘッダー、フッターコンポーネントを削除

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,13 @@
 import Profile from '../components/profile'
-import Header from "../components/header";
-import Footer from "../components/footer";
 
 export default function MyApp() {
   return (
     <>
-      <Header />
       <div>
         <main>
           <Profile />
         </main>
       </div>
-      <Footer />
     </>
   )
 }
